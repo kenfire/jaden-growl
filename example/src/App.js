52cb2a72a02b90a6212f738065deeee0fc053a1b
kenfire <11364148+kenfire@users.noreply.github.com>
2020-07-25 22:35:53 +1000

    Migrate code for npm

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
-import { ExampleComponent } from 'growl'
-import 'growl/dist/index.css'
+import { Link } from 'react-router-dom'
+import { useGrowl, Growl } from 'growl'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+import 'growl/dist/index.css'
+import logo from './logo.svg'
+
+function App() {
+  const [active, setActive] = useGrowl()
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Link className='App-link' to='/other'>
+          Go to the Other component
+        </Link>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <a className='App-link' href='#' onClick={() => void setActive(true)}>
+          Clik here to activate the growl
+        </a>
+      </header>
+      <Growl
+        onDismissed={() => setActive(false)}
+        active={active}
+        message='Hello World!'
+      />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import React from 'react'
-import styles from './styles.module.css'
+import './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Growl = ({ active, message, onDismissed }) => {
+  const [state, setState] = React.useState(true)
+
+  React.useEffect(() => {
+    // Dismiss notification after 3 seconds
+    setTimeout(() => {
+      setState(false)
+    }, 3000)
+
+    setState(true)
+  }, [active])
+
+  const handleClick = () => {
+    setState(false)
+    onDismissed()
+  }
+
+  return (
+    <div className={`growl${active && state ? ' active' : ''}`}>
+      {message}
+      <div onClick={handleClick} className='growl-close'></div>
+    </div>
+  )
+}
+
+export function useGrowl() {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false)
+
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active)
+    }
+  ]
 }
